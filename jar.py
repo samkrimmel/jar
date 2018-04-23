9960a786acd6f0052cae3ed7a2bc00851176aad8
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-04-23 12:49:27 -0400

    Updated from Brython Server: 4/23/2018 12:49:14 PM

--- jar.py
+++ jar.py
@@ -10,4 +10,14 @@ w = int(input('Prize if correct guess, between 3 and 15: '))
 Red = randint(0,N)
 Green = N-Red
 
+PR = Red/N
+PG = Green/N
+
+D = (-1*W*(PR))/(-1*(PG))
+
+print(Red)
+print(Green)
+print(PR)
+print(PG)
+print(D)
 
