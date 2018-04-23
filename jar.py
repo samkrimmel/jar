34d9769867396e249c3674ff19dba4d0bf92046c
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-04-23 12:38:37 -0400

    Updated from Brython Server: 4/23/2018 12:38:24 PM

--- jar.py
+++ jar.py
@@ -2,4 +2,12 @@
 #4/23/18
 #jar.py - Precalc Project
 
+from random import randint
+
+N = int(input('Number of marbles, between 8 and 20: '))
+w = int(input('Prize if correct guess, between 3 and 15: '))
+
+Red = randint(0,N)
+Green = N-Red
+
 
