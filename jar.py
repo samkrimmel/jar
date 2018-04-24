b96be48e8dd4696b49cf464b8b0a3a6972952fe1
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-04-24 12:28:40 -0400

    Updated from Brython Server: 4/24/2018 12:28:27 PM

--- jar.py
+++ jar.py
@@ -7,6 +7,8 @@ from random import randint
 
 N = 8 #int(input('Number of marbles, between 8 and 20: '))
 W = 3 #int(input('Prize if correct guess, between 3 and 15: '))
+runs = 10
+
 
 Red = randint(0,N)
 Green = N-Red
@@ -14,11 +16,21 @@ Green = N-Red
 Pr = Red/N
 Pg = Green/N
 
-D = (-1*W*(Pr))/(-1*(Pg))
+for num in range(0,runs):
+    if Pg == 0:
+        D = W
+    else:
+        D = (W*Pr)
+    total += D
+    num += 1
+    
+avg = total/runs
+
 
 print('Red:',Red)
 print('Green:',Green)
 print('PR:',Pr)
 print('PG:',Pg)
 print('Price:',D)
+print('Avg: ',avg)
 
