cc7563128242254367c03d0cd5caefbea2d99be9
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-04-26 12:30:13 -0400

    Updated from Brython Server: 4/26/2018 12:30:12 PM

--- Strat3.py
+++ Strat3.py
@@ -1,1 +1,32 @@
+#Sam Krimmel
+#4/26/18
+#Strat3.py
+
+from random import randint
+
+N = 10 #int(input('Number of marbles, between 8 and 20: '))
+W = 8 #int(input('Prize if correct guess, between 3 and 15: '))
+runs = 10000
+
+#EXPERIMENTAL
+
+EtotalWins = 0
+EtotalRuns = 0
+
+for i in range(0,runs):
+    
+    ERed = randint(0,N)
+    EGreen = N-ERed
+    
+    EPr = ERed/N
+    EPg = EGreen/N
+    
+    EMarble = randint(1,N)
+    EMarble2 = randint(1,N)
+    
+    if EMarble = EMarble2:
+        EtotalWins += 1
+        EtotalRuns += 1
+
+EPw = EtotalWins/EtotalRuns
 
