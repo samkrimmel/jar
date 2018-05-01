b66042847ced2b46a2d080b4384794c6da2a2d1f
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-05-01 14:49:50 -0400

    Updated from Brython Server: 5/1/2018 2:49:46 PM

--- Strat5.py
+++ Strat5.py
@@ -24,6 +24,8 @@ for i in range(1,N+1):
     else:
         marbles.append('R')
 
+print(marbles)
+
 for i in range(0,runs):
     
     result = ''
@@ -33,23 +35,32 @@ for i in range(0,runs):
     N -= 1
     del marbles[Emarble]
     
+    print()
+    print(marbles)
+    
     Emarble2 = randint(0,N-1)
     result += str(marbles[Emarble2])
     N -= 1
     del marbles[Emarble2]
     
+    print()
     print(marbles)
-    print(result)
     
     Emarble3 = randint(0,N-1)
     result += str(marbles[Emarble3])
     N -= 1
     del marbles[Emarble3]
+
+    print()
+    print(marbles)
     
     Emarble4 = randint(0,N-1)
     result += str(marbles[Emarble4])
     N -= 1
     del marbles[Emarble4]
+
+    print()
+    print(marbles)
     
     if result == 'RRRR' or result == 'RRRG' or result == 'RGRR' or result == 'RGGG' or result == 'GGGG' or result == 'GGGR' or result == 'GRRR' or result == 'GRGG':
         EtotalWins += 1
