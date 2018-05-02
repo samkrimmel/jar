3152208940f9a70c47386a8e320fb3a1564a83d6
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-05-02 10:49:01 -0400

    Updated from Brython Server: 5/2/2018 10:48:55 AM

--- Strat5.py
+++ Strat5.py
@@ -43,7 +43,6 @@ for i in range(0,runs):
     
     if result == 'RR' or result == 'GG':
         N += 2
-        print(N)
         Emarble3 = randint(0,N-1)
         result += str(marbles[Emarble3])
     else:
@@ -53,7 +52,6 @@ for i in range(0,runs):
         del marbles2[Emarble3]
         
         N += 3
-        print(N)
         Emarble4 = randint(0,N-1)
         result += str(marbles[Emarble4])
 
