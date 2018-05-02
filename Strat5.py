334918dd3a8f5954d862ca66740e3bd895b7527b
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-05-02 12:16:03 -0400

    Updated from Brython Server: 5/2/2018 12:15:57 PM

--- Strat5.py
+++ Strat5.py
@@ -6,7 +6,7 @@ from random import randint
 
 W = 8 #int(input('Prize if correct guess, between 3 and 15: '))
 N = 10 #int(input('Number of marbles, between 8 and 20: '))
-runs = 1000 #int(input('Experimental runs: '))
+runs = 100 #int(input('Experimental runs: '))
 
 #EXPERIMENTAL
 
@@ -58,6 +58,7 @@ for i in range(0,runs):
 
     if result == 'RRR' or result == 'RGRR' or result == 'RGGG' or result == 'GGG' or result == 'GRRR' or result == 'GRGG':
         EtotalWins += 1
+        print(result)
     EtotalRuns += 1
     
     Etotal += W*EPr
