4a7afa8798d80d55738b02e4d407d1dfdbdf1148
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-05-03 14:20:59 -0400

    Updated from Brython Server: 5/3/2018 2:20:52 PM

--- Strat5.py
+++ Strat5.py
@@ -6,7 +6,7 @@ from random import randint
 
 W = 8 #int(input('Prize if correct guess, between 3 and 15: '))
 N = 10 #int(input('Number of marbles, between 8 and 20: '))
-runs = 1000 #int(input('Experimental runs: '))
+runs = 1 #int(input('Experimental runs: '))
 
 #EXPERIMENTAL
 
@@ -20,12 +20,11 @@ for i in range(0,runs):
     
     result = ''
 
-    for n in range(1,N+1):
-        mcolor = randint(1,2)
-        if mcolor == 1:
-            marbles.append('G')
-        else:
-            marbles.append('R')
+    ERed = randint(0,N)
+    EGreen = N - ERed
+    
+    marbles = ['R']*Ered + ['G']*EGreen
+    print(marbles)
     
     EPr = marbles.count('R')/len(marbles)
     
